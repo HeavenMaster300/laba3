158c14eaf499d4f6728b9e14e22ed47650f8228c
HeavenMaster300 <bogdanvarlam2@gmail.com>
2025-10-03 15:07:07 +0300

    Zadanie 3

--- zad1.py
+++ zad1.py
@@ -1,9 +1,37 @@
+#создайте список квадратов чисел от 1 до 10 с использованием list comprehension
 squares = [x**2 for x in range(1, 11)]
-print(squares)
+print(f"\nСписок квадратов числе от 1 до 10 {squares}\n")
 
+#с помощью list comprehesion получите список только четных чисел из диапозона range(1,20)
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
-print(even_numbers)
+print(f"\nСписок четных чисел в диапазоне от 1 до 20 {even_numbers}\n")
 
+#Дан список слов words = ['python', 'Java', 'c++', 'Rust', 'go']
+#Создайте новый список, где все слова будут в верхнем регистре и длиннее 3 слов
 words = ['python', 'Java', 'c++', 'Rust', 'go']
 upper_long_words = [word.upper() for word in words if len(word) > 3]
-print(upper_long_words)
+print(f"\nСписок, где все слова в верхнем регистре и длиннее 3 слов {upper_long_words}\n")
+
+#Создайте класс-итератор Countdown, который принимает число n и при итерации возвращает числа от n до 1
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+        self.current = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < 1:
+            raise StopIteration
+        value = self.current
+        self.current -= 1
+        return value
+
+# Принятие числа от пользователя
+n = int(input("Введите число n для создания итерации от n до 1: "))
+
+# Использование итератора
+print(f'Итерация от {n} до 1')
+for num in Countdown(n):
+    print(num)
