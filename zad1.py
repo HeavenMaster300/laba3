452daece361d7e0eee9d5199ac8f94ddad4ade2a
HeavenMaster300 <bogdanvarlam2@gmail.com>
2025-10-03 14:53:09 +0300

    Zadanie 3

--- zad1.py
+++ zad1.py
@@ -3,3 +3,7 @@ print(squares)
 
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 print(even_numbers)
+
+words = ['python', 'Java', 'c++', 'Rust', 'go']
+upper_long_words = [word.upper() for word in words if len(word) > 3]
+print(upper_long_words)
